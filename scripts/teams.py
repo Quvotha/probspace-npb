2fc418741ba7100ec46b2894e0d21869372484c2
Quvotha <nndropout1+github@gmail.com>
2021-05-23 00:42:22 +0900

    Use bgTop and bgBottom instead of enum

--- scripts/teams.py
+++ scripts/teams.py
@@ -1,61 +1,22 @@
-import enum
-
 import pandas as pd
 
 
-@enum.unique
-class Teams(enum.IntEnum):
-    HIROSHIMA = 1
-    CHUNICHI = 2
-    HANSHIN = 3
-    KYOJIN = 4
-    DeNA = 5
-    YAKURUTO = 6
-    LOTTE = 7
-    RAKUTEN = 8
-    NICHIHAMU = 9
-    ORIX = 10
-    SOFTBANK = 11
-    SEIBU = 12
+class Teams(object):
 
-    @classmethod
-    def get_team_map(cls) -> dict:
-        return {
-            'DeNA': Teams.DeNA,
-            'ソフトバンク': Teams.SOFTBANK,
-            '阪神': Teams.HANSHIN,
-            'ヤクルト': Teams.YAKURUTO,
-            'オリックス': Teams.ORIX,
-            'ロッテ': Teams.LOTTE,
-            '楽天': Teams.RAKUTEN,
-            '西武': Teams.SEIBU,
-            '日本ハム': Teams.NICHIHAMU,
-            '中日': Teams.CHUNICHI,
-            '広島': Teams.HIROSHIMA,
-            '巨人': Teams.KYOJIN,
-        }
-
-    @classmethod
-    def to_int(cls, team: str) -> int:
-        team_map = cls.get_team_map()
-        if team not in team_map.keys():
-            raise ValueError(f'Cannot convert {team}')
-        return team_map[team]
-
-    @classmethod
-    def extract_batter_team(cls, s: pd.Series) -> int:
+    @staticmethod
+    def extract_batter_team(s: pd.Series) -> int:
         if s.isBottom == 1:
-            return cls.to_int(s.bottomTeam)
+            return s.bgBottom
         elif s.isBottom == 0:
-            return cls.to_int(s.topTeam)
+            return s.bgTop
         else:
             raise ValueError(f'`isBottom` must be one of [0, 1] but {s.isBottom} given')
 
-    @classmethod
-    def extract_pitcher_team(cls, s: pd.Series) -> int:
+    @staticmethod
+    def extract_pitcher_team(s: pd.Series) -> int:
         if s.isBottom == 1:
-            return cls.to_int(s.topTeam)
+            return s.bgTop
         elif s.isBottom == 0:
-            return cls.to_int(s.bottomTeam)
+            return s.bgBottom
         else:
             raise ValueError(f'`isBottom` must be one of [0, 1] but {s.isBottom} given')
--- scripts/test_teams.py
+++ scripts/test_teams.py
@@ -8,82 +8,43 @@ from teams import Teams
 class TestTeams(unittest.TestCase):
 
     def test_extract_pitcher_team(self):
-        input_ = pd.Series(
-            {'topTeam': '中日',
-             'bottomTeam': '阪神',
-             'isBottom': 0
-             }
-        )
-        expected = Teams.HANSHIN
+        input_ = pd.Series({
+            'bgTop': 3,
+            'bgBottom': 2,
+            'isBottom': 0
+        })
+        expected = input_.bgBottom
         output = Teams.extract_pitcher_team(input_)
         self.assertEqual(output, expected)
 
         input_ = pd.Series(
-            {'topTeam': '中日',
-             'bottomTeam': '阪神',
+            {'bgTop': 3,
+             'bgBottom': 2,
              'isBottom': 1
              }
         )
-        expected = Teams.CHUNICHI
+        expected = input_.bgTop
         output = Teams.extract_pitcher_team(input_)
         self.assertEqual(output, expected)
-
-        input_ = pd.Series(
-            {'topTeam': '中日',
-             'bottomTeam': 'マリナーズ',
-             'isBottom': 0
-             }
-        )
-        with self.assertRaises(ValueError):
-            output = Teams.extract_pitcher_team(input_)
-
-        input_ = pd.Series(
-            {'topTeam': 'ヤンキース',
-             'bottomTeam': '阪神',
-             'isBottom': 1
-             }
-        )
-        with self.assertRaises(ValueError):
-            output = Teams.extract_pitcher_team(input_)
 
     def test_extract_batter_team(self):
-        input_ = pd.Series(
-            {'topTeam': '中日',
-             'bottomTeam': '阪神',
-             'isBottom': 0
-             }
-        )
-        expected = Teams.CHUNICHI
+        input_ = pd.Series({
+            'bgTop': 6,
+            'bgBottom': 7,
+            'isBottom': 0
+        })
+        expected = input_.bgTop
         output = Teams.extract_batter_team(input_)
         self.assertEqual(output, expected)
 
-        input_ = pd.Series(
-            {'topTeam': '中日',
-             'bottomTeam': '阪神',
-             'isBottom': 1
-             }
-        )
-        expected = Teams.HANSHIN
+        input_ = pd.Series({
+            'bgTop': 6,
+            'bgBottom': 7,
+            'isBottom': 1
+        })
+        expected = input_.bgBottom
         output = Teams.extract_batter_team(input_)
         self.assertEqual(output, expected)
-
-        input_ = pd.Series(
-            {'topTeam': '中日',
-             'bottomTeam': 'マリナーズ',
-             'isBottom': 1
-             }
-        )
-        with self.assertRaises(ValueError):
-            output = Teams.extract_batter_team(input_)
-
-        input_ = pd.Series(
-            {'topTeam': 'ヤンキース',
-             'bottomTeam': '阪神',
-             'isBottom': 0
-             }
-        )
-        with self.assertRaises(ValueError):
-            output = Teams.extract_batter_team(input_)
 
 
 if __name__ == '__main__':
